7ede7bb9943f02e71ae934b71be24e18ed1feb3f
Jrtqq <149610449+Jrtqq@users.noreply.github.com>
2025-01-24 14:49:39 +0300

    Health Component

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [field:SerializeField] public float MaxHealth { get; private set; }
+
+    public float Health { get; private set; }
+
+    public event Action Died;
+    public event Action Damaged;
+    public event Action Healed;
+
+    private void OnValidate()
     {
-        
+        Mathf.Clamp(MaxHealth, 0, float.MaxValue);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Awake()
     {
-        
+        if (MaxHealth == 0)
+        {
+            Debug.Warning($"{nameof(MaxHealth)} is not initialized");
+
+            Died?.Invoke();
+        }
+
+        Health = MaxHealth;
+    }
+
+    public void ApplyDamage(float value)
+    {
+        if (value < 0) return;
+
+        Health = Mathf.Clamp(Health - value, 0, MaxHealth);
+
+        if (Health == 0)
+            Died?.Invoke();
+        else
+            Damaged?.Invoke();
+    }
+
+    public void Heal(float value)
+    {
+        Health += Mathf.Clamp(value, 0, float.maxValue);
+
+        Healed?.Invoke();
     }
 }
--- Assets/Scripts/Player/Mover.cs
+++ Assets/Scripts/Player/Mover.cs
@@ -1,101 +1,104 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
-
-public class Mover : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private float _speed;
-    [SerializeField] private float _jumpForce;
-    [SerializeField] private float _sensitivity = 1f;
-    [SerializeField] private float _maxCameraTilt = 5f;
-    [SerializeField] private float _cameraTiltSpeed = 5f;
-    [SerializeField] private LayerMask _groundCheckMask;
+    [RequireComponent(typeof(CharacterController))]
 
-    [SerializeField] Transform _camera;
-    [SerializeField] SphereCollider _groundChecker;
-    [SerializeField] BoxCollider _coyoteChecker;
-    [SerializeField] BoxCollider _wallChecker;
-    private CharacterController _controller;
-    private Transform _transform;
-
-    private InputActions _input;
-
-    private float _yVelocity = 0;
-    private float _cameraAngleX;
-    private float _cameraAngleZ;
-
-    private void Awake()
+    public class Mover : MonoBehaviour
     {
-        _input = new();
-        _controller = GetComponent<CharacterController>();
-        _transform = transform;
+        [SerializeField] private float _speed;
+        [SerializeField] private float _jumpForce;
+        [SerializeField] private float _sensitivity = 1f;
+        [SerializeField] private float _maxCameraTilt = 5f;
+        [SerializeField] private float _cameraTiltSpeed = 5f;
+        [SerializeField] private LayerMask _groundCheckMask;
 
-        _cameraAngleX = _camera.localEulerAngles.x;
-        _cameraAngleZ = _camera.localEulerAngles.z;
-    }
+        [SerializeField] Transform _camera;
+        [SerializeField] SphereCollider _groundChecker;
+        [SerializeField] BoxCollider _coyoteChecker;
+        [SerializeField] BoxCollider _wallChecker;
+        private CharacterController _controller;
+        private Transform _transform;
 
-    private void OnEnable()
-    {
-        _input.Enable();
+        private InputActions _input;
 
-        _input.Player.Jump.performed += GetJumpImpulse;
-    }
+        private float _yVelocity = 0;
+        private float _cameraAngleX;
+        private float _cameraAngleZ;
 
-    private void OnDisable()
-    {
-        _input.Disable();
-
-        _input.Player.Jump.performed -= GetJumpImpulse;
-    }
-
-    private void Update()
-    {
-        ProcessMovement();
-        RotateCamera();
-    }
-
-    private void ProcessMovement()
-    {
-        Vector2 input = _input.Player.Move.ReadValue<Vector2>();
-        Vector3 direction = _transform.right * input.x + _transform.forward * input.y;
-
-        _controller.Move(new Vector3(direction.x * _speed, _yVelocity, direction.z * _speed) * Time.deltaTime);
-
-        if (_controller.isGrounded == false)
+        private void Awake()
         {
-            _yVelocity += Physics.gravity.y;
-        }
-        else
-        {
-            _yVelocity = -1;
+            _input = new();
+            _controller = GetComponent<CharacterController>();
+            _transform = transform;
+
+            _cameraAngleX = _camera.localEulerAngles.x;
+            _cameraAngleZ = _camera.localEulerAngles.z;
         }
 
-        _cameraAngleZ = Mathf.Lerp(_cameraAngleZ, _maxCameraTilt * -input.x, _cameraTiltSpeed * Time.deltaTime);
-        _camera.localEulerAngles = new Vector3(
-            _camera.localEulerAngles.x, 
-            _camera.localEulerAngles.y,
-            _cameraAngleZ);
-    }
+        private void OnEnable()
+        {
+            _input.Enable();
 
-    private void RotateCamera()
-    {
-        Vector2 pointerDelta = _input.Player.Look.ReadValue<Vector2>();
+            _input.Player.Jump.performed += GetJumpImpulse;
+        }
 
-        _cameraAngleX = Mathf.Clamp(_cameraAngleX - pointerDelta.y * _sensitivity * Time.deltaTime, -89, 89);
-        _camera.localEulerAngles = new Vector3(
-            _cameraAngleX,
-            _camera.localEulerAngles.y,
-            _camera.localEulerAngles.z);
+        private void OnDisable()
+        {
+            _input.Disable();
 
-        _transform.Rotate(Vector3.up * pointerDelta.x * _sensitivity * Time.deltaTime);
-    }
+            _input.Player.Jump.performed -= GetJumpImpulse;
+        }
 
-    private void GetJumpImpulse(InputAction.CallbackContext context)
-    {
-        if (Physics.CheckSphere(_groundChecker.transform.position, _groundChecker.radius, _groundCheckMask)
-            || Physics.CheckBox(_coyoteChecker.transform.position, _coyoteChecker.size / 2f, _transform.rotation, _groundCheckMask)
-            && Physics.CheckBox(_wallChecker.transform.position, _wallChecker.size / 2f, _transform.rotation, _groundCheckMask) == false)
-            _yVelocity = _jumpForce;
+        private void Update()
+        {
+            ProcessMovement();
+            RotateCamera();
+        }
+
+        private void ProcessMovement()
+        {
+            Vector2 input = _input.Player.Move.ReadValue<Vector2>();
+            Vector3 direction = _transform.right * input.x + _transform.forward * input.y;
+
+            _controller.Move(new Vector3(direction.x * _speed, _yVelocity, direction.z * _speed) * Time.deltaTime);
+
+            if (_controller.isGrounded == false)
+            {
+                _yVelocity += Physics.gravity.y;
+            }
+            else
+            {
+                _yVelocity = -1;
+            }
+
+            _cameraAngleZ = Mathf.Lerp(_cameraAngleZ, _maxCameraTilt * -input.x, _cameraTiltSpeed * Time.deltaTime);
+            _camera.localEulerAngles = new Vector3(
+                _camera.localEulerAngles.x,
+                _camera.localEulerAngles.y,
+                _cameraAngleZ);
+        }
+
+        private void RotateCamera()
+        {
+            Vector2 pointerDelta = _input.Player.Look.ReadValue<Vector2>();
+
+            _cameraAngleX = Mathf.Clamp(_cameraAngleX - pointerDelta.y * _sensitivity * Time.deltaTime, -89, 89);
+            _camera.localEulerAngles = new Vector3(
+                _cameraAngleX,
+                _camera.localEulerAngles.y,
+                _camera.localEulerAngles.z);
+
+            _transform.Rotate(Vector3.up * pointerDelta.x * _sensitivity * Time.deltaTime);
+        }
+
+        private void GetJumpImpulse(InputAction.CallbackContext context)
+        {
+            if (Physics.CheckSphere(_groundChecker.transform.position, _groundChecker.radius, _groundCheckMask)
+                || Physics.CheckBox(_coyoteChecker.transform.position, _coyoteChecker.size / 2f, _transform.rotation, _groundCheckMask)
+                && Physics.CheckBox(_wallChecker.transform.position, _wallChecker.size / 2f, _transform.rotation, _groundCheckMask) == false)
+                _yVelocity = _jumpForce;
+        }
     }
 }
